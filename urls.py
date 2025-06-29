bdbcfbd42628e3f631202f4cab000d1257c431d1
Artyom Shkuratov <@.>
2025-06-29 13:30:39 +0300

    Создан файл с эндпоинтами

---
+++ urls.py
@@ -0,0 +1,14 @@
+class Urls:
+    BASE_URL = "https://stellarburgers.nomoreparties.site"
+
+    # url для пользователя
+    CREATE_USER = f"{BASE_URL}/api/auth/register"
+    LOGIN_USER = f"{BASE_URL}/api/auth/login"
+    USER_DATA = f"{BASE_URL}/api/auth/user"  
+    # url для заказов
+    CREATE_ORDER = f"{BASE_URL}/api/orders"  
+    USER_ORDERS = f"{BASE_URL}/api/orders"   
+    # url для ингредиентов
+    INGREDIENTS = f"{BASE_URL}/api/ingredients"
+    
+    
