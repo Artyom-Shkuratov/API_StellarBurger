c5683309070021d3933b6036488b87981873e21b
Artyom Shkuratov <@.>
2025-06-29 15:19:44 +0300

    Добавлен url

--- urls.py
+++ urls.py
@@ -4,7 +4,8 @@ class Urls:
     # url для пользователя
     CREATE_USER = f"{BASE_URL}/api/auth/register"
     LOGIN_USER = f"{BASE_URL}/api/auth/login"
-    USER_DATA = f"{BASE_URL}/api/auth/user"  
+    USER_DATA = f"{BASE_URL}/api/auth/user" 
+    DELETE_USER = f"{BASE_URL}/auth/user" 
     # url для заказов
     CREATE_ORDER = f"{BASE_URL}/api/orders"  
     USER_ORDERS = f"{BASE_URL}/api/orders"   
