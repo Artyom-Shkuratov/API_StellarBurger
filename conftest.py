ee090cdc0d071e16227e2a41d4a7a5ce3845e4ab
Artyom Shkuratov <@.>
2025-07-06 12:36:38 +0300

    Исрпавлена фикстура на регистрацию

--- conftest.py
+++ conftest.py
@@ -1,21 +1,21 @@
 import random
 import pytest
-from helpers import GeneratorData, User, Order
+from helpers import GeneratorData
+from logic.user_logic import User
+from logic.order_logic import Order
 
 
 @pytest.fixture
-def registered_user():
-        random_user_data = GeneratorData.generate_payload()
-        User.register_user(random_user_data)
-        yield random_user_data
-        User.delete_user(random_user_data)
-        
-        
+def registered_user(random_user):
+    User.register_user(random_user)
+    yield random_user
+    User.delete_user(random_user)
+      
 @pytest.fixture
 def random_user():
-    user_data=GeneratorData.generate_payload
+    user_data = GeneratorData.generate_payload()
     yield user_data
-    User.delete_user(user_data)
+
 
 @pytest.fixture
 def get_random_ingridients():
